1be38647c45963d53c1cf3223c54805e5d42b2d3
hizamakura <kaworinakano@gmail.com>
2019-06-24 21:09:57 +0800

    Initial commit

---
+++ Furniture/Furniture/Bootstrapper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Caliburn.Micro;
+
+namespace Furniture
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        private readonly IWindowManager manager = new WindowManager();
+
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            // manager.ShowWindow();
+        }
+    }
+}
