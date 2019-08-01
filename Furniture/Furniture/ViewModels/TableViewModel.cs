a9df918fca99b411bd8da98f73e134a1a5381919
loliprotector <kaworinakano@gmail.com>
2019-08-01 09:48:31 +0800

    Allow drag and drop in Image

--- Furniture/Furniture/ViewModels/TableViewModel.cs
+++ Furniture/Furniture/ViewModels/TableViewModel.cs
@@ -1,7 +1,12 @@
-﻿using System.Linq;
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Windows;
+using System.Windows.Media;
 using System.Windows.Media.Effects;
+using System.Windows.Media.Imaging;
 using Caliburn.Micro;
 using Furniture.Properties;
 using Furniture.Relationship;
@@ -13,6 +18,24 @@ namespace Furniture.ViewModels
     public class TableViewModel : Parent
     {
         private readonly WindowManager _windowManager = new WindowManager();
+
+        public BitmapImage Image { get; set; } = BitmapToImageSource(Properties.Resources.furniture);
+
+        public static BitmapImage BitmapToImageSource(Bitmap bitmap)
+        {
+            using (MemoryStream memory = new MemoryStream())
+            {
+                bitmap.Save(memory, System.Drawing.Imaging.ImageFormat.Bmp);
+                memory.Position = 0;
+                BitmapImage bitmapimage = new BitmapImage();
+                bitmapimage.BeginInit();
+                bitmapimage.StreamSource = memory;
+                bitmapimage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapimage.EndInit();
+
+                return bitmapimage;
+            }
+        }
 
         public event WoodCreatedDelegate WoodCreated;
 
@@ -72,6 +95,37 @@ namespace Furniture.ViewModels
         {
             OrdersView.Remove(itemViewModel);
             OnPropertyChanged(null);
+        }
+
+        [UsedImplicitly]
+        public void ChangeImage(DragEventArgs e)
+        {
+            var fileList = (string[]) e.Data.GetData(DataFormats.FileDrop, false);
+            if (fileList?.FirstOrDefault() != null)
+            {
+                Image = new BitmapImage(new Uri(fileList.First()));
+            }
+        }
+
+        [UsedImplicitly]
+        public void FilePreviewDragEnter(DragEventArgs e)
+        {
+            var dropEnabled = true;
+            if (e.Data.GetDataPresent(DataFormats.FileDrop, true))
+            {
+                if (e.Data.GetData(DataFormats.FileDrop, true) is string[] filenames)
+                {
+                    var ext = Path.GetExtension(filenames[0])?.ToUpperInvariant();
+                    if (ext != ".PNG" && ext != ".JPG" && ext != ".JPEG") dropEnabled = false;
+                }
+            }
+            else { dropEnabled = false; }
+
+            if (!dropEnabled)
+            {
+                e.Effects = DragDropEffects.None;
+                e.Handled = true;
+            }
         }
     }
 }
